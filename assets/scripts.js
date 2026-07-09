c6b67cea2161964192d1024e383b1e31ec0a5e34
Ethano41414 <ethano.vanacker7@gmail.com>
2026-07-09 22:27:46 +0200

    1.1.5

---
+++ assets/scripts.js
@@ -0,0 +1,16 @@
+let nombre_clicks=0;
+
+const bouton_actualiseur=document.getElementById("hided_button");
+
+bouton_actualiseur.addEventListener("click",function() {
+    nombre_clicks++;
+    if (4>nombre_clicks && nombre_clicks>=3) {
+        bouton_actualiseur.textContent="Actualiser";
+        bouton_actualiseur.style.opacity="1";
+        bouton_actualiseur.style.width="6rem";
+    };
+    if (nombre_clicks>=4) {
+        bouton_actualiseur.textContent="Act";
+    }
+});
+
